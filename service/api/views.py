44c376560e8cedd310495a883b87c68e0b9cb6fc
Griffone <thegriffones@gmail.com>
2022-02-19 17:31:07 +0100

    Require auth for apartment view

--- service/api/views.py
+++ service/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions, viewsets
 
 from api import models, serializers
 
 
-class ApartmentViewSet(ModelViewSet):
+class ApartmentViewSet(viewsets.ModelViewSet):
+    # Commented out as Greg was too lazy to figure out token auth during setup
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = models.Apartment.objects.all()
     serializer_class = serializers.ApartmentSerializer
